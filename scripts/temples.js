0b1ad36f7439c7f74f1bd2147863e8155251d1c7
euge86 <eugeespejo@gmail.com>
2024-11-29 13:29:31 -0300

    week2

---
+++ scripts/temples.js
@@ -0,0 +1,26 @@
+const currentyear = document.querySelector("#currentyear");
+
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+currentyear.innerHTML = today.getFullYear();
+
+lastModified.innerHTML = document.lastModified;
+
+
+//Hamburger button//
+
+const menuButton = document.getElementById('menu');
+const navMenu = document.getElementById('nav-menu');
+
+    menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('active'); // 
+});
+
+
+
+
+
+
+
